7c628e50ccd723bd60f68833825c2aabda4aaaba
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2024-12-16 12:24:10 +0700

    feat: change & create route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use App\Livewire\Admin\AddCandidate as AdminAddCandidate;
 use App\Livewire\Admin\AddSchedule as AdminAddSchedule;
+use App\Livewire\Admin\AddSerialNumber as AdminAddSerialNumber;
+use App\Livewire\Admin\Candidates as AdminCandidates;
 use App\Livewire\Admin\Dashboard as AdminDashboard;
+use App\Livewire\Admin\EditCandidate as AdminEditCandidate;
+use App\Livewire\Admin\EditSchedule as AdminEditSchedule;
+use App\Livewire\Admin\EditSerialNumber as AdminEditSerialNumber;
 use App\Livewire\Admin\Schedule as AdminSchedule;
+use App\Livewire\Admin\SerialNumber as AdminSerialNumber;
 use App\Livewire\Candidates;
 use App\Livewire\Dashboard;
 use App\Livewire\Login;
@@ -32,13 +39,31 @@ Route::get('/quick-count', QuickCount::class)->name('quick-count');
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth:web', AdminMiddleware::class]], function () {
     Route::get('/', AdminDashboard::class)->name('dashboard');
 
-    Route::get('/schedules', AdminSchedule::class)->name('schedules');
-    Route::get('/add-schedule', AdminAddSchedule::class)->name('schedules.add');
+    Route::group(['prefix' => 'schedules', 'as' => 'schedules'], function () {
+        Route::get('/', AdminSchedule::class);
+        Route::get('/add', AdminAddSchedule::class)->name('.add');
+        Route::get('/{schedule}', AdminEditSchedule::class)->name('.edit');
+    });
+
+    Route::group(['prefix' => 'serial-numbers', 'as' => 'serial-numbers'], function () {
+        Route::get('/', AdminSerialNumber::class);
+        Route::get('/add', AdminAddSerialNumber::class)->name('.add');
+        Route::get('/{number}', AdminEditSerialNumber::class)->name('.edit');
+    });
+
+    Route::group(['prefix' => 'candidates', 'as' => 'candidates'], function () {
+        Route::get('/', AdminCandidates::class);
+        Route::get('/add', AdminAddCandidate::class)->name('.add');
+        Route::get('/{candidate}', AdminEditCandidate::class)->name('.edit');
+    });
 });
 
-Route::middleware('auth:web')->group(function(){
-    Route::get('/logout', function(){
+Route::middleware('auth:web')->group(function () {
+    Route::get('/vote', Vote::class)->name('vote');
+
+    Route::get('/logout', function () {
         auth()->guard('web')->logout();
+
         return redirect()->route('login');
     })->name('logout');
 });
